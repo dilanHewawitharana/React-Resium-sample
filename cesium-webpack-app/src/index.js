d546f72fc87e1591c951c8e914eb82ccd035f6b0
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2021-06-10 23:43:17 +0900

    cesium hello world

--- cesium-webpack-app/src/index.js
+++ cesium-webpack-app/src/index.js
@@ -1,1 +1,5 @@
-console.log('Hello World!');
+var Cesium = require('cesium/Cesium');
+require('./css/main.css');
+require('cesium/Widgets/widgets.css');
+
+var viewer = new Cesium.Viewer('cesiumContainer');
--- cesium-webpack-app/webpack.config.js
+++ cesium-webpack-app/webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopywebpackPlugin = require('copy-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+// The path to the CesiumJS source code
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
 
 module.exports = {
     context: __dirname,
@@ -12,19 +18,83 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
+
+        // Needed to compile multiline strings in Cesium
+        sourcePrefix: ''
+    },
+    amd: {
+        // Enable webpack-friendly use of require in Cesium
+        toUrlUndefined: true
+    },
+    // node: {
+    //     // Resolve node module use of fs
+    //     fs: 'empty'
+    // },
+    resolve: {
+        alias: {
+            // CesiumJS module name
+            cesium: path.resolve(__dirname, cesiumSource)
+        }
     },
     module: {
-        rules: [{
+        rules: [
+        {
             test: /\.css$/,
             use: [ 'style-loader', 'css-loader' ]
-        }, {
+        }, 
+        {
             test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
             use: [ 'url-loader' ]
-        }]
+        },
+        {
+            // Strip cesium pragmas
+            test: /\.js$/,
+                enforce: 'pre',
+                include: path.resolve(__dirname, cesiumSource),
+                use: [{
+                    loader: 'strip-pragma-loader',
+                    options: {
+                        pragmas: {
+                            debug: false
+                        }
+                    }
+                }]
+        },
+        // {
+        //     test: /\.css$/,
+        //     use: [
+        //         'style-loader',
+        //         {
+        //             loader: 'css-loader',
+        //             options: {
+        //                 // minify loaded css
+        //                 minimize: true
+        //             }
+        //         }
+        //     ]
+        // }
+        ]
     },
+    devtool: 'eval',
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html'
+        }),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'cesium',
+        //     minChunks: module => module.context && module.context.indexOf('cesium') !== -1
+        // }),
+        // Copy Cesium Assets, Widgets, and Workers to a static directory
+        new CopywebpackPlugin({ 
+            patterns: [
+                { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
+                { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
+                { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }
+            ]
+        }),
+        new webpack.DefinePlugin({
+            // Define relative base path in cesium for loading assets
+            CESIUM_BASE_URL: JSON.stringify('')
         })
     ],
     devServer: {
